b5f4980c0846d83b1dafe69a60d246bce14911a4
manshisiliconstream-blip <manshi.siliconstream@gmail.com>
2026-04-07 10:23:34 +0530

    Update login.js
    add comment

--- Full_Automation_Playwright/tests/Pages/login.js
+++ Full_Automation_Playwright/tests/Pages/login.js
@@ -1,20 +1,21 @@
-exports.Loginpage= class Loginpage {
+exports.Loginpage = class Loginpage {
+    // added here
 
-    constructor(page){
-        this.page= page
-        this.email_textbox= page.getByPlaceholder('admin@clinic.com')
+    constructor(page) {
+        this.page = page
+        this.email_textbox = page.getByPlaceholder('admin@clinic.com')
         this.password_textbox = page.locator('input[type="password"]')
-        this.Submit_button= page.getByRole('button', {name:'Submit'})
+        this.Submit_button = page.getByRole('button', { name: 'Submit' })
     }
-    async gotoLoginpage(){
-         await this.page.goto('http://ec2-13-48-105-24.eu-north-1.compute.amazonaws.com/')
-         await this.page.getByText('Login / SignUp').first().click();
+    async gotoLoginpage() {
+        await this.page.goto('http://ec2-13-48-105-24.eu-north-1.compute.amazonaws.com/')
+        await this.page.getByText('Login / SignUp').first().click();
     }
-     async login(email, password){
+    async login(email, password) {
         await this.email_textbox.fill(email)
         await this.password_textbox.fill(password)
         await this.Submit_button.click()
-   
+
     }
-    
+
 }
