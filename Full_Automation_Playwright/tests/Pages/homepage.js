1b83baa68e7f847924919de8eb1acb6f260a50f4
manshisiliconstream-blip <manshi.siliconstream@gmail.com>
2026-04-07 10:17:24 +0530

    Update homepage.js
    added comment

--- Full_Automation_Playwright/tests/Pages/homepage.js
+++ Full_Automation_Playwright/tests/Pages/homepage.js
@@ -1,20 +1,21 @@
 import { expect } from '@playwright/test';
-export class HomePage{
-    constructor(page){
-        this.page= page;
-        this.logout_btn= this.page.getByText('Logout');
-        this.getstarted_btn= this.page.getByText('Get Started');
-        this.bookdemo_btn= this.page.getByText('Book a Demo');
-        this.title = page.getByText('PhysioCare', {exact: 'true'});
-        this.tagline = page.getByText('THE CLINICAL SANCTUARY');
-        this.securityText = page.getByText('Enterprise-grade security');
-    
-    }
-    async  gotohome(){
-    await this.page.goto('http://ec2-13-48-105-24.eu-north-1.compute.amazonaws.com/')
-}
+//home page
+export class HomePage {
+  constructor(page) {
+    this.page = page;
+    this.logout_btn = this.page.getByText('Logout');
+    this.getstarted_btn = this.page.getByText('Get Started');
+    this.bookdemo_btn = this.page.getByText('Book a Demo');
+    this.title = page.getByText('PhysioCare', { exact: 'true' });
+    this.tagline = page.getByText('THE CLINICAL SANCTUARY');
+    this.securityText = page.getByText('Enterprise-grade security');
 
-  async logout(){
+  }
+  async gotohome() {
+    await this.page.goto('http://ec2-13-48-105-24.eu-north-1.compute.amazonaws.com/')
+  }
+
+  async logout() {
     await this.logout_btn.click();
   }
 
@@ -23,14 +24,14 @@ export class HomePage{
     await expect(this.tagline).toBeVisible();
   }
   async clickGetStarted() {
-  await expect(this.getstarted_btn).toBeVisible();
-  await this.getstarted_btn.click();
-}
+    await expect(this.getstarted_btn).toBeVisible();
+    await this.getstarted_btn.click();
+  }
 
   async clickBookDemo() {
-  await expect(this.bookdemo_btn).toBeVisible();
-  await this.bookdemo_btn.click();
-}
+    await expect(this.bookdemo_btn).toBeVisible();
+    await this.bookdemo_btn.click();
+  }
 
   async scrollDown() {
     await this.page.mouse.wheel(0, 1000);
